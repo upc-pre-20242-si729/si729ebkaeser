bbbb3b9a5b256a6b86ae226b5e822ad87228c9cd
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-25 22:31:53 -0500

    refactor(inventory): change the data type of the parameter in the repository query.

--- src/main/java/com/kaeser/platform/inventory/infrastructure/persistence/jpa/repositories/EquipmentRepository.java
+++ src/main/java/com/kaeser/platform/inventory/infrastructure/persistence/jpa/repositories/EquipmentRepository.java
@@ -1,6 +1,7 @@
 package com.kaeser.platform.inventory.infrastructure.persistence.jpa.repositories;
 
 import com.kaeser.platform.inventory.domain.model.aggregates.Equipment;
+import com.kaeser.platform.inventory.domain.model.valueobjects.MaterialSerialNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,5 +19,5 @@ public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
      * @param materialSerialNumber The serial number of the equipment
      * @return True if an equipment with the given serial number exists, false otherwise
      */
-    boolean existsByMaterialSerialNumber(String materialSerialNumber);
+    boolean existsByMaterialSerialNumber(MaterialSerialNumber materialSerialNumber);
 }
