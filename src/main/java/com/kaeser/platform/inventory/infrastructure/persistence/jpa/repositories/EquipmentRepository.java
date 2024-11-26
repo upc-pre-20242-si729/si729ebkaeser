885719e1bc99cee72200c9d8ca12384067b246e4
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-25 22:21:28 -0500

    feat(inventory): add equipment repository.

---
+++ src/main/java/com/kaeser/platform/inventory/infrastructure/persistence/jpa/repositories/EquipmentRepository.java
@@ -0,0 +1,22 @@
+package com.kaeser.platform.inventory.infrastructure.persistence.jpa.repositories;
+
+import com.kaeser.platform.inventory.domain.model.aggregates.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * EquipmentRepository
+ * @summary
+ * JPA Repository for Equipment entity
+ */
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+
+    /**
+     * @summary
+     * Check if an equipment with the given serial number exists
+     * @param materialSerialNumber The serial number of the equipment
+     * @return True if an equipment with the given serial number exists, false otherwise
+     */
+    boolean existsByMaterialSerialNumber(String materialSerialNumber);
+}
